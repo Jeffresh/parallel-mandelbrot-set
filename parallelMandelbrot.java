fb9d1f2060e7d7a534bbd2840baa661d4257d1bd
Jeff <jeff.uca@gmail.com>
2020-05-19 09:59:35 +0200

    :art: change format and method/variables names
    deleting ref errors

--- parallelMandelbrot.java
+++ parallelMandelbrot.java
@@ -7,9 +7,7 @@ import javax.swing.JPanel;
 
 import com.sun.tools.javac.Main;
 
-
-public class parallelMandelbrot implements Runnable
-{
+public class parallelMandelbrot implements Runnable {
 
   private static int MAX_ITER = 100000;
   private static double ZOOM = 150;
@@ -21,28 +19,25 @@ public class parallelMandelbrot implements Runnable
   private int iter;
   private int realnucleos;
 
-
-
-  private static boolean parar =false;
-  private static MainCanvas canvas;
-  // public static JPanel grafica_ref;
-
-
-
+  private static boolean parar = false;
+  private static MainCanvas canvasRef;
+  public static AnalyticsMultiChart population_chart_ref;
 
   private static int tamPool;
   private int tarea;
-  private int in,fn;
+  private int in, fn;
   private static int numtareas;
   private static ThreadPoolExecutor miPool;
   private static CyclicBarrier barrera = null;
 
   private static int[] alives;
-//  public static XYSeries[] series;
+  //  public static XYSeries[] series;
 
+  public void plug(MainCanvas ref) {
+    canvasRef = ref;
+  }
 
-  public void ini_bm(int z, int mi)
-  {
+  public void ini_bm(int z, int mi) {
 
     width = 800;
     height = 600;
@@ -52,165 +47,118 @@ public class parallelMandelbrot implements Runnable
     ZOOM = z;
     MAX_ITER = mi;
 
-    alives = new int [2];
-    alives[0] =0;
+    alives = new int[2];
+    alives[0] = 0;
     alives[1] = 0;
-
-
   }
 
-
-  public int[] get_alives()
-  {
-      return alives;
+  public int[] get_alives() {
+    return alives;
   }
 
-  public void stop()
-  {
+  public void stop() {
     miPool.shutdownNow();
-    parar=true;
+    parar = true;
   }
 
-  public void start()
-  {
-    parar =false;
+  public void start() {
+    parar = false;
   }
 
-  public void enchufa(MainCanvas c
-//          , XYSeries[] s
-  )
-  {
-    canvas=c;
+  public void run() {
 
-    // series = new XYSeries[8];
-    // series=s;
+    // aqui comienza la rutina a paralelizar
+    for (int y = in; y < fn; y++) {
+      if (parar) break;
+
+      for (int x = 0; x < width; x++) {
+        if (parar) break;
+
+        zx = zy = 0;
+        cX = (x - 400) / ZOOM;
+        cY = (y - 300) / ZOOM;
+        iter = MAX_ITER;
+        while (zx * zx + zy * zy < 4 && iter > 0) {
+          if (parar) break;
+
+          tmp = zx * zx - zy * zy + cX;
+          zy = 2.0 * zx * zy + cY;
+          zx = tmp;
+          iter--;
+        }
+
+        Imagen.setRGB(x, y, iter | (iter << 8));
+        canvasRef.validate();
+        canvasRef.repaint();
+      }
+    } // aqui finaliza la rutina a paralelizar
   }
 
+  public static void next_gen_concurrent(int nt) {
 
-
-
-  public void run()
-  {
-
-   //aqui comienza la rutina a paralelizar
-   for (int y = in; y < fn; y++) 
-   {
-     if(parar)
-            break;
-          
-     for (int x = 0; x < width; x++) 
-     {
-       if(parar)
-          break;
-          
-
-         
-           zx = zy = 0;
-           cX = (x - 400) / ZOOM;
-           cY = (y - 300) / ZOOM;
-           iter = MAX_ITER;
-           while (zx * zx + zy * zy < 4 && iter > 0)
-          {
-             if(parar)
-                break;
-          
-                 tmp = zx * zx - zy * zy + cX;
-                 zy = 2.0 * zx * zy + cY;
-                 zx = tmp;
-                 iter--;
-          }
-          
-          Imagen.setRGB(x, y, iter | (iter << 8));
-          canvas.validate();
-          canvas.repaint();
-
-
-     }
-   }//aqui finaliza la rutina a paralelizar
-
-  }
-
-  public static void next_gen_concurrent(int nt)
-  {
-
-    tamPool =nt;
+    tamPool = nt;
 
     // barrera = new CyclicBarrier (tamPool);
     numtareas = tamPool;
 
-    miPool = new ThreadPoolExecutor(
-                      tamPool, tamPool, 60000L,
-                      TimeUnit.MILLISECONDS,
-                      new LinkedBlockingQueue<Runnable>());
-    parallelMandelbrot[] tareas = new  parallelMandelbrot[nt];
+    miPool =
+        new ThreadPoolExecutor(
+            tamPool, tamPool, 60000L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<Runnable>());
+    parallelMandelbrot[] tareas = new parallelMandelbrot[nt];
 
-    for(int t = 0; t < nt; t++)
-    {
-      tareas[t] = new parallelMandelbrot(t+1);
-        miPool.execute(tareas[t]);
-
+    for (int t = 0; t < nt; t++) {
+      tareas[t] = new parallelMandelbrot(t + 1);
+      miPool.execute(tareas[t]);
     }
 
-
     miPool.shutdown();
-    try{
-    miPool.awaitTermination(10, TimeUnit.HOURS);}catch(Exception e){}
-
-
+    try {
+      miPool.awaitTermination(10, TimeUnit.HOURS);
+    } catch (Exception e) {
+    }
   }
 
+  public parallelMandelbrot(int i) {
+    tarea = i;
 
-  public parallelMandelbrot(int i)
-  {
-      tarea = i;
+    int paso = height / numtareas;
 
-      int paso = height/numtareas;
+    fn = paso * tarea;
+    in = fn - paso;
 
+    if (numtareas == tarea) fn = height;
 
-      fn = paso * tarea;
-      in = fn - paso;
-
-      if( numtareas == tarea)
-          fn =height;
-
-      System.out.println(in+" "+fn);   
-
+    System.out.println(in + " " + fn);
   }
 
-  public parallelMandelbrot(int z, int mi) 
-  {
+  public parallelMandelbrot(int z, int mi) {
 
     width = 800;
     height = 600;
-    
+
     Imagen = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
 
     // realnucleos = Runtime.getRuntime().availableProcessors();
 
     // System.out.println(realnucleos);
 
-
-
     ZOOM = z;
     MAX_ITER = mi;
-    alives = new int [2];
-    alives[0] =0;
+    alives = new int[2];
+    alives[0] = 0;
     alives[1] = 0;
 
     // long times[] = new long[4];
 
-
     // for(int i = 0 ; i < 4; i++)
-    // { 
+    // {
 
     //   long startTime = System.currentTimeMillis();
-      
 
     //   next_gen_concurrent(i+1);
-     
-      
+
     //   long endTime = System.currentTimeMillis();
-      
+
     //   times[i] =endTime - startTime;
     // }
 
@@ -218,6 +166,4 @@ public class parallelMandelbrot implements Runnable
     //   System.out.println(time+'\n');
 
   }
-
-
 }
